7cd02b44d871effe8a246e4345b4c31bff8b7465
Mark Taguiad <marktaguiad@marktaguiad.dev>
2026-02-20 07:32:18 +0800

    minor tweak warp

--- app/themes/heavycactus/static/js/warp.js
+++ app/themes/heavycactus/static/js/warp.js
@@ -3,8 +3,13 @@ let sky, center
 function dot(i) {
    const size = Math.round(Math.random() + 1)
    const root = document.createElement('span')
+
    root.style.top = center.y + 'px'
    root.style.left = center.x + 'px'
+
+   // randomize starting opacity timing so we don’t get a blank frame
+   root.style.animationDelay = `${Math.random() * -30}s, ${Math.random() * -30}s`
+
    root.classList.add('star', `size-${size}`, `axis-${i}`)
    return root
 }
@@ -15,12 +20,21 @@ function clear() {
 
 function init() {
    sky = document.querySelector('#sky')
+   if (!sky) return
+
    center = {
       x: sky.clientWidth / 2,
       y: sky.clientHeight / 2,
    }
+
    clear()
-   for (let i = 0; i < 360; i++) sky.appendChild(dot(i))
+
+   const frag = document.createDocumentFragment()
+   for (let i = 0; i < 360; i++) {
+      frag.appendChild(dot(i))
+   }
+   sky.appendChild(frag)
 }
 
-window.onload = init
+window.addEventListener('load', init)
+window.addEventListener('resize', init)
